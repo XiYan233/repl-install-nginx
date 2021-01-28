77f82d8d0f83e90ab7f033065033a2daa9597175
Demontisa <49113774+Demontisa@users.noreply.github.com>
2021-01-28 14:13:32 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,36 +25,51 @@ cd pcre-8.43
 echo -e "\033[32m 正在编译pcre \033[0m"
 make
 echo -e "\033[32m 完成 \033[0m"
+echo -e "\033[32m 休息3秒 \033[0m"
+sleep 3s
 echo -e "\033[32m 正在安装pcre \033[0m"
 make install
 echo -e "\033[32m 完成 \033[0m"
+echo -e "\033[32m 休息3秒 \033[0m"
+sleep 3s
 #编译安装openssl
 cd ../openssl-1.0.2s
 ./config    --prefix=/home/runner/${name}/openssl
 echo -e "\033[32m 正在编译openssl \033[0m"
 make
 echo -e "\033[32m 完成 \033[0m"
+echo -e "\033[32m 休息3秒 \033[0m"
+sleep 3s
 echo -e "\033[32m 正在安装openssl \033[0m"
 make install
 echo -e "\033[32m 完成 \033[0m"
+echo -e "\033[32m 休息3秒 \033[0m"
+sleep 3s
 #编译安装zlib
 cd ../zlib-1.2.11
 ./configure    --prefix=/home/runner/${name}/zlib
 echo -e "\033[32m 正在编译zlib \033[0m"
 make
 echo -e "\033[32m 完成 \033[0m"
+echo -e "\033[32m 休息3秒 \033[0m"
+sleep 3s
 echo -e "\033[32m 正在安装zlib \033[0m"
 make install
 echo -e "\033[32m 完成 \033[0m"
+echo -e "\033[32m 休息3秒 \033[0m"
+sleep 3s
 #编译安装nginx
 cd ../nginx-1.16.1
 ./configure  --with-http_stub_status_module  --with-http_ssl_module  --prefix=/home/runner/${name}/nginx  --with-pcre=/home/runner/${name}/pcre-8.43  --with-openssl=/home/runner/${name}/openssl-1.0.2s  --with-zlib=/home/runner/${name}/zlib-1.2.11
 echo -e "\033[32m 正在编译nginx \033[0m"
 make
 echo -e "\033[32m 完成 \033[0m"
+echo -e "\033[32m 休息3秒 \033[0m"
+sleep 3s
 echo -e "\033[32m 正在安装nginx \033[0m"
 make install
 echo -e "\033[32m 完成 \033[0m"
+
 
 echo -e "安装完成，请手动修改nginx/conf/nginx.conf里的listen 值大于1024"
 
